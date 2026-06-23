dac5ca221d295ee28fb16b20f6e9b10bdfc3db4b
AnastasiPapathanasiou <anpapathanasiou@feedthebet.com>
2026-06-23 13:25:24 +0300

    Returns only combo odds

---
+++ file_reader_combo.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+def read_txt(filepath):
+    with open(filepath, 'r', encoding = "utf-8") as f:
+        for line in f:
+            print(line.strip())
+            input()
+
+def read_csv(filepath):
+    with open(filepath, 'r', encoding = "utf-8") as f:
+        for line in f:
+            print(line.strip())
+            input()
+
+def combo_odds(filepath):
+    with open (filepath, 'r', encoding = "utf-8") as f:
+        next (f)
+        for line in f:
+            columns = line.strip().split(',')
+            combo_odds = columns[3]
+            print(combo_odds)
+
+READERS = {".txt": read_txt, ".csv": read_csv}
+
+def main():
+    filepath = input("Give the name of the file: ").strip()
+    if not os.path.exists(filepath):
+        print ("File not found")
+        sys.exit(1)
+
+    _,ext = os.path.splitext(filepath)
+    ext = ext.lower()
+
+    reader = READERS.get(ext)
+    if reader is None:
+        print ("File not supported")
+        sys.exit(1)
+
+    print(f"File: {filepath}")
+    print("=" * 30)
+    combo_odds(filepath)
+
+main()
